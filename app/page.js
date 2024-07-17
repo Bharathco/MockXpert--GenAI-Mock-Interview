cf7a8f233c50d1dbfa3103622b96f6fa7add5259
bharath.rajashekar3@gmail.com <bharath.rajashekar3@gmail.com>
2024-07-17 12:33:25 +0530

    updated home page design

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="absolute inset-0 bg-black opacity-75"></div>
       <div className="relative z-10 text-white text-center">
       <h1 className="text-4xl md:text-6xl font-semibold mb-5">
-  Welcome to <span className='text-[#ba4040]'>MockXpert</span>
+  Welcome to <span className='text-[#dc4848]'>MockXpert</span>
 </h1>
 
         <p className="text-lg mb-10 max-w-2xl">
@@ -17,7 +17,7 @@ export default function Home() {
           Practice answering common interview questions and receive feedback to improve your performance.
         </p>
         <Link href="/dashboard">
-          <Button className="px-6 py-3 text-lg font-medium text-white bg-[#ba4040] rounded-lg hover:bg-[#ba4040]">
+          <Button className="px-6 py-3 text-lg font-medium text-white bg-[#dc4848] rounded-lg hover:bg-[#dc4848]">
             Go to Dashboard
           </Button>
         </Link>
